c9f8f94deed6e189727c5e855ae10288588d890a
Mateus Almeida <mateusalmeida559@gmail.com>
2023-01-15 20:26:11 -0300

    Primeiro commit

---
+++ src/test/java/robos/GaiaRobo.java
@@ -0,0 +1,14 @@
+package robos;
+
+public class GaiaRobo {
+    public void abrirChrome() {
+        System.out.println("Estou abrindo o Chrome!");
+    }
+    public void fazerLogin() {
+        System.out.println("Vai fazer login!");
+    }
+
+    public void lancarNota() {
+        System.out.println("Vai lancar nota!");
+    }
+}
